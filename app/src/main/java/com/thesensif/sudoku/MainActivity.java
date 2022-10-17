85dab094edbf375536855d20b33214806c427051
TheSensif <sergisimon7@gmail.com>
2022-10-17 19:16:25 +0200

    Initial commit

---
+++ app/src/main/java/com/thesensif/sudoku/MainActivity.java
@@ -0,0 +1,14 @@
+package com.thesensif.sudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
